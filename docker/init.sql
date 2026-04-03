618ae0e53f6fcb97c88a12cc9ed5b95ae4ff47e8
DQuang23 <nduyquang04@gmail.com>
2026-04-03 11:37:57 +0700

    init setup

---
+++ docker/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS orders (
+    order_id UUID PRIMARY KEY,
+    customer_id UUID,
+    order_status VARCHAR(50),
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
