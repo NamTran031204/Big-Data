5b61d68118d606024ac76ae25d4bca0e4a22146b
Tran Nam <tpnam1w3@gmail.com>
2026-06-10 14:33:58 +0700

    fix loi datetime khi gui kafka

---
+++ SpringBoot/src/main/java/com/example/BigData/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.example.BigData.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        // dùng với Instant
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+}
